a2a60a3d9a9bf172efe7d7a634b902b73e95c02d
vagrant <vagrant@homestead>
2026-07-14 10:27:05 -0500

    API 基础配置

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->appendToGroup('web', \App\Http\Middleware\EnsureEmailIsVerified::class);
         $middleware->appendToGroup('web', \App\Http\Middleware\RecordLastActivedTime::class);
+        $middleware->prependToGroup('api', \App\Http\Middleware\AcceptHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
